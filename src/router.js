9c77987819e146f01099a6039cd9b964bd54e207
Erika-Agana <agan0003@algonquinlive.com>
2019-08-07 15:11:21 -0400

    Complete app

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import SignUp from "./views/SignUp.vue";
+import SignIn from "./views/SignIn.vue";
+import Dashboard from "./views/Dashboard.vue";
+
+import store from "./store";
 
 Vue.use(Router);
 
@@ -14,13 +19,38 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/signup",
+      name: "signup",
+      component: SignUp
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: SignIn
+    },
+    { path: "/signIn",
+    name: "signIn",
+    component: SignIn,
+    beforeEnter(to, from, next) {
+      if (!store.state.idToken) {
+          next();
+      } else {
+          next("/dashboard");
+      }
+  }
+  },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+      // 如果sign in，则可以进入dashboard页面，如果没有，则不能进入
+      beforeEnter(to, from, next) {
+        if (store.state.idToken) {
+            next();
+        } else {
+            next("/signin");
+        }
+    }
     }
   ]
 });
--- src/store.js
+++ src/store.js
@@ -1,10 +1,193 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axiosAuth from "./axios-auth";
+import router from "./router";
+import { isContext } from "vm";
+import globalAxios from "axios";
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    idToken: null,
+    userId: null,
+    error: "",
+    user: null
+  },
+
+
+  mutations: {
+    AUTH_USER(state, userData) {
+      state.idToken = userData.token;
+      state.userId = userData.userId;
+    },
+    SET_ERROR(state, errorMessage) {
+      state.error = errorMessage;
+    },
+    EMPTY_ERROR(state) {
+      state.error = "";
+    },
+    CLEAR_DATA(state) {
+      state.idToken = null;
+      state.userId = null;
+    },
+    STORE_USER(state, user) {
+      state.user = user;
+    }
+  },
+
+
+  actions: {
+    signUp({commit, dispatch}, authData ) {
+      axiosAuth
+        // Web API key
+        // AIzaSyAQ7HOfFVPb-gDdIdpIg_baxqR0niwX6gg
+        .post("accounts:signUp?key=AIzaSyAQ7HOfFVPb-gDdIdpIg_baxqR0niwX6gg", 
+        {
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        })
+        .then(res => {
+          console.log(res);
+          // Save the auth info in the state
+          commit("AUTH_USER", {
+            token: res.data.idToken,
+            userId: res.data.localId
+          });
+
+          // Local Storage
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+
+          localStorage.setItem("token",res.data.idToken)
+          localStorage.setItem("userId",res.data.localId)
+          localStorage.setItem("userId","expirationDate", expirationDate);
+
+          localStorage.setItem("userEmail", authData.email);
+
+          dispatch("storeUser", authData);
+
+          router.push({ name: "dashboard" });
+
+        })
+        .catch(error => {
+          console.log(error.response.data.error.message);
+          commit("SET_ERROR", error.response.data.error.message);
+        });
+      },
+
+      signIn({ commit }, authData) {
+        axiosAuth
+          .post(
+            "accounts:signInWithPassword?key=AIzaSyAQ7HOfFVPb-gDdIdpIg_baxqR0niwX6gg",
+            {
+              email: authData.email,
+              password: authData.password,
+              returnSecureToken: true
+            }
+          )
+          .then(res => {
+            console.log(res);
+            commit("AUTH_USER", {
+              token: res.data.idToken,
+              userId: res.data.localId
+            });
+
+            // Local Storage
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+
+          localStorage.setItem("token",res.data.idToken)
+          localStorage.setItem("userId",res.data.localId)
+          localStorage.setItem("userId","expirationDate", expirationDate);
+
+          localStorage.setItem("userEmail", authData.email);
+
+          router.push({ name: "dashboard" });
+
+
+          })
+          .catch(error => {
+            console.log(error.response.data.error.message);
+            commit("SET_ERROR", error.response.data.error.message);
+          });
+      },// closing sign in
+
+      clearError({commit}) {
+        commit ('EMPTY_ERROR');
+      },
+
+      logout ({commit}) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('expirationDate');
+        localStorage.removeItem('userId');
+
+        // commit mutation to clear the state
+        commit("CLEAR_DATA");
+        // send user to sign in routh
+        router.push({ name:"signin"});
+      },
+      autoLogin({ commit }) {
+        const token = localStorage.getItem("token");
+        const expirationDate = localStorage.getItem("expirationDate");
+        const userId = localStorage.getItem("userId")
+
+        const now = new Date();
+
+        if (now >= expirationDate) {
+          return;
+        }
+        commit("AUTH_USER", {
+          token: token, 
+          userId: userId
+        });
+      },
+      storeUser({ state }, userData){
+        if(!state.idToken) {
+          return;
+        }
+        axiosAuth.post("https://agan0003-week12.firebaseio.com/users.json" + "?auth=" + state.idToken, userData)
+        .then(res => console(res))
+        .catch(error => console.log(error.message));
+      },
+      fetchUser({ commit, state }, userEmail){
+        if(!state.idToken) {
+          return;
+        }
+        globalAxios.get("https://agan0003-week12.firebaseio.com/users.json" + "?auth=" + state.idToken)
+        .then(res => {
+          const data = res.data;
+          for (let key in data){
+            const user = data[key];
+            if(user.email == userEmail) {
+              console.log(user);
+              user.id = key;
+              commit("STORE_USER", user)
+            }
+          }
+        });
+      },
+      updateUser({ state }){
+        globalAxios.patch("https://agan0003-week12.firebaseio.com/users/" + 
+        state.user.id + 
+        ".json" + 
+        "?auth=" + 
+        state.idToken, 
+        { name: state.user.name }
+        )
+        .then(res => console.log(res))
+        .catch(error => console.log(error.response));
+      }
+
+      },
+      getters: {
+        isauthenticated(state) {
+          return state.idToken !== null;
+        },
+        getUser(state) {
+          return state.user;
+        }
+  }
 });
